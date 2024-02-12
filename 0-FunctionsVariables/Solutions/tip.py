812a9207feab08fff84042fe54da4f3ac4b8b830
Ethan Czapla <124547419+EthnCZ@users.noreply.github.com>
2024-02-12 00:28:57 +0000

    fix

--- 0-FunctionsVariables/Solutions/tip.py
+++ 0-FunctionsVariables/Solutions/tip.py
@@ -1,25 +1,23 @@
 def main():  # Main function
     print("How much was the meal?")  # ask for meal price
     D = D_float(input())  # response by user
-    print(D)
     print("What percentage would you like to tip?")  # ask for tip (in percent)
-    P = D_float(input())  # response by user
-    print(P)
+    P = P_float(input())  # response by user
     T = D * P  # math for total
     print(f"Leave ${T:.2f}")  # print total
 
 
 def D_float(d):  # function for price
-    d = d.replace("$", " ")
-    d = float(d)
-    d = round(d, 2)
+    d = d.replace("$", " ")  # removes $
+    d = float(d)  # stores as float
+    d = round(d, 2)  # rounds to 2 dp.
     return d
 
 
 def P_float(p):  # function for tip
-    p = p.replace("%", " ")
-    p = float(p)
-    p = p / 100
+    p = p.replace("%", "")  # removes %
+    p = float(p)  # stores as float
+    p = p / 100  # to decimal
     return p
 
 
